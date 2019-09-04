f308fc4cde2e6e21a413438499adc6e75a84e076
J. Cooper <cooperjstn@gmail.com>
2019-09-04 17:28:32 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Tetris from './components/Tetris'
+import 'tachyons/css/tachyons.min.css'
+
+const App= () => (
+ <div className ="fl w-100 pa2 center">
+  <div className="App">
+    <Tetris/>
+  </div>
+  </div>
+
+
+); 
+export default App
