a4ea519be0b1da6a728cfa5843bfc2dba31e6360
J. Cooper <cooperjstn@gmail.com>
2019-09-04 22:11:10 -0400

    Updates

---
+++ precache-manifest.3b8dd657d6dee8c1a108440c9e09e1e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a70250d0b3a875de9d0952a56b73947",
+    "url": "/tetris-react-hooks/index.html"
+  },
+  {
+    "revision": "0d8b5ccab6f2add30157",
+    "url": "/tetris-react-hooks/static/css/2.1688eb18.chunk.css"
+  },
+  {
+    "revision": "961b57c948707553a77f",
+    "url": "/tetris-react-hooks/static/css/main.8d1cf24e.chunk.css"
+  },
+  {
+    "revision": "0d8b5ccab6f2add30157",
+    "url": "/tetris-react-hooks/static/js/2.9a8a2b29.chunk.js"
+  },
+  {
+    "revision": "961b57c948707553a77f",
+    "url": "/tetris-react-hooks/static/js/main.802f469f.chunk.js"
+  },
+  {
+    "revision": "39c0a977b4a6c5cbe4f8",
+    "url": "/tetris-react-hooks/static/js/runtime~main.70642a4d.js"
+  }
+]);
--- src/components/Cell.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { StyledCell } from './styles/StyledCell.js'
-import { TETROMINOS } from '../tetrominos.js'
-
-const Cell = ({ type }) => (
-    <StyledCell type ={type} color={TETROMINOS[type].color}/>
-);
-export default React.memo(Cell)
